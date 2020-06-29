36d0a7e4faa699f3940a1ae5a6d7c3d34d750d90
Brian Adams <brian.adams@eso.com>
2020-06-29 17:43:06 -0400

    internal types need to be public across a nuget package?

--- src/Schlechtums.FastDynamicAccess/Schlechtums.FastDynamicAccess/Types/PopulateData.cs
+++ src/Schlechtums.FastDynamicAccess/Schlechtums.FastDynamicAccess/Types/PopulateData.cs
@@ -4,38 +4,38 @@ using System.Text;
 
 namespace Schlechtums.FastDynamicAccess.Types
 {
-    internal class PopulateData
+    public class PopulateData
     {
         /// <summary>
         /// The model property names in repsective order to the column names.
         /// </summary>
-        internal List<String> MappedCols { get; set; }
+        public List<String> MappedCols { get; set; }
 
         /// <summary>
         /// The names of the columns capitalized.
         /// </summary>
-        internal List<String> ColUpperNames { get; set; }
+        public List<String> ColUpperNames { get; set; }
 
         /// <summary>
         /// The number of columns in the data set.
         /// </summary>
-        internal int ColCount { get; set; }
+        public int ColCount { get; set; }
 
         /// <summary>
         /// List of booleans representing whether or not the model property has an accessible setter.
         /// </summary>
-        internal List<Boolean> HasSetters { get; set; }
+        public List<Boolean> HasSetters { get; set; }
 
         /// <summary>
         /// List of property types.
         /// </summary>
-        internal List<Type> PropertyTypes { get; set; }
+        public List<Type> PropertyTypes { get; set; }
 
         /// <summary>
         /// List of property write String.Format format strings.
         /// </summary>
-        internal List<String> PropertyFormats { get; set; }
+        public List<String> PropertyFormats { get; set; }
 
-        internal List<int> FDAIndexes { get; set; }
+        public List<int> FDAIndexes { get; set; }
     }
 }
--- src/Schlechtums.FastDynamicAccess/Schlechtums.FastDynamicAccess/Types/PropertyAccessors.cs
+++ src/Schlechtums.FastDynamicAccess/Schlechtums.FastDynamicAccess/Types/PropertyAccessors.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
 
-[assembly: InternalsVisibleTo("System.Data.DBAccess.Generic")]
 namespace Schlechtums.FastDynamicAccess.Types
 {
-    internal class PropertyAccessors
+    public class PropertyAccessors
     {
         /// <summary>
         /// Boolean if the property has an accessible getter or not.
         /// </summary>
-        internal Boolean HasGetter { get; set; }
+        public Boolean HasGetter { get; set; }
 
         /// <summary>
         /// Boolean if the property has an accessible setter or not.
         /// </summary>
-        internal Boolean HasSetter { get; set; }
+        public Boolean HasSetter { get; set; }
     }
 }
